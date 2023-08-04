60d8a2b342e54bc68da52e90415096ee2a6b481a
pourya <pouryakarami10@gmail.com>
2023-08-04 17:28:23 +0330

    gitignore

--- README.md
+++
@@ -1,1 +0,0 @@
-# Roshan Internship TakeNews Project
--- api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import NewsList
-
-app_name = "api"
-urlpatterns = [
-    path("", NewsList.as_view(), name="list")
-]
