a3f218536719b161efc7ddcf49be2b6b6a0a7264
pourya22334415 <pouryakarami10@gmail.com>
2023-08-15 16:30:02 +0330

    challange2

--- TakeNews/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from news.models import New
-
-class NewsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = New
-        fields = '__all__'
